b3710598ed278aaa262165e443d9c0926bdc4afd
Oleg Chursin <oleg.chursin@aon.com>
2019-08-17 18:32:48 -0400

    Update Photography.jsx

--- src/components/Photography.jsx
+++ src/components/Photography.jsx
@@ -4,14 +4,14 @@ const Photography = () => {
   return (
     <section>
       <h2>Photography</h2>
-      <h4>Portraits</h4>
+      <h4>People Alone</h4>
       <div>
-        <a href="http://portraits.olegchursin.com">portraits.olegchursin.com</a>
+        <a href="http://portraits.olegchursin.com">portraits-olegchursin</a>
       </div>
 
-      <h4>Wedding</h4>
+      <h4>People Together</h4>
       <div>
-        <a href="http://wedding.olegchursin.com">wedding.olegchursin.com</a>
+        <a href="https://wedding-olegchursin.netlify.com">wedding-olegchursin</a>
       </div>
     </section>
   )
